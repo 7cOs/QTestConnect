6e1096e98efaa96b048907b75944b31166f8d9bd
SKARNEH <SKARNEH@PC0QMY9T.cwc.cbi.net>
2018-05-04 15:35:56 -0500

    Revert "Added observation methods (for purposes of analysis) to include:"
    This reverts commit 982b3d3e6df396c48595a4446f1a9bce1b0ab211.

--- qtestConn/conn2qtest/src/test/conn2qtest/QTestConnect.java
+++ qtestConn/conn2qtest/src/test/conn2qtest/QTestConnect.java
@@ -3,20 +3,18 @@ package test.conn2qtest;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+
 import org.qas.qtest.api.auth.PropertiesQTestCredentials;
 import org.qas.qtest.api.auth.QTestCredentials;
+import org.qas.qtest.api.internal.model.Link;
 import org.qas.qtest.api.services.design.TestDesignService;
 import org.qas.qtest.api.services.design.TestDesignServiceClient;
 import org.qas.qtest.api.services.design.model.ListTestCaseRequest;
-import org.qas.qtest.api.services.design.model.ListTestStepRequest;
 import org.qas.qtest.api.services.design.model.TestCase;
-import org.qas.qtest.api.services.design.model.TestStep;
 import org.qas.qtest.api.services.project.ProjectService;
 import org.qas.qtest.api.services.project.ProjectServiceClient;
 import org.qas.qtest.api.services.project.model.GetModuleRequest;
@@ -29,7 +27,6 @@ public class QTestConnect {
 	public static final String QTEST_PROPS_PATH = "./qTestCredentials.properties";
 	public static Properties qTestProps = null;
 	public static ProjectService projectService = null;
-	public static long projectId;
 
 	static {
 		try {
@@ -38,19 +35,16 @@ public class QTestConnect {
 			x.printStackTrace();
 		}
 	}
-
+	
 	public static void initialise() throws Exception {
+		
 		qTestProps = new Properties(); 
 		qTestProps.load( new FileInputStream(QTEST_PROPS_PATH) );
 		
-		// - Establish connection to project end-point - //
 		connectToProjectService();
 		
-		// - Set project Id - //
-		setProjectId();
-		
 		System.out.println("Process initialsed - " + 
-				QTestConnect.class.getCanonicalName() + "...");
+				QTestConnect.class.getCanonicalName()+"...");
 	}
 	
 	public static QTestCredentials getCredentials() throws IOException {
@@ -62,11 +56,20 @@ public class QTestConnect {
 	public static void connectToProjectService() throws IOException {
 		projectService = new ProjectServiceClient(getCredentials());
 		projectService.setEndpoint(qTestProps.getProperty("serviceEndPoint"));
-		System.out.println("Connected to ProjectService: " + projectService);
-	}
-	
-	public static void setProjectId() {
-		projectId = getProjectId(qTestProps.getProperty("defaultProject"));
+		System.out.println( "Connected to ProcessService: " + projectService );
+		/*ListModuleRequest listModuleRequest = new ListModuleRequest();
+		listModuleRequest.withProjectId(68329L).withIncludeDescendants(true);
+		System.out.println( "-- Modules --" );
+		List<org.qas.qtest.api.services.project.model.Module> ms = 
+				projectService.listModule(listModuleRequest);
+		for(org.qas.qtest.api.services.project.model.Module m : ms ) {
+			System.out.println("Name: "+m.getName()+", Id: "+m.getId()+", pId: "+m.getPid());
+			// - Isolate - //
+			if( m.getName().equals("To Be Automated") ) {
+				System.out.println( m.getChildren().size() );
+				break;
+			}
+		}*/
 	}
 	
 	public static void displayProjects() {
@@ -90,7 +93,27 @@ public class QTestConnect {
 			System.out.println("");
 		}
 	}
-
+	
+	public static void observeTestDesignService() throws IOException {
+		QTestCredentials credentials = 
+				new PropertiesQTestCredentials(new File(QTEST_PROPS_PATH));
+		TestDesignService testDesignService = new TestDesignServiceClient(credentials);
+		// - Attempt to list test cases - //
+		ListTestCaseRequest listTestCaseRequest = new ListTestCaseRequest();
+		listTestCaseRequest
+			.withProjectId(68329L)
+			.withModuleId( 4823877L );
+		
+		List<TestCase> testcases = testDesignService.listTestCase(listTestCaseRequest);
+		System.out.println( "TestCase(s)" );
+		for( TestCase testcase : testcases ) {
+			System.out.println( "Name: " + testcase.getName() );
+			System.out.println( "Id: " + testcase.getId() );
+			System.out.println( "URL: " + testcase.getWebUrl() );
+		}
+	}
+	
+	
 	public static long getProjectId(String name) {
 		long pId = -1;
 		ListProjectRequest listProjectRequest = new ListProjectRequest();
@@ -105,112 +128,53 @@ public class QTestConnect {
 		return pId;
 	}
 	
-	public static long getModuleId(String name) {
+	public static long getModuleId(String name, long pId) {
 		long mId = -1;
-		ListModuleRequest listModuleRequest = new ListModuleRequest();
-		listModuleRequest.withProjectId(projectId).withIncludeDescendants(true);
-		List<org.qas.qtest.api.services.project.model.Module> modules = 
-				projectService.listModule(listModuleRequest);
-		for(org.qas.qtest.api.services.project.model.Module module : modules) {
-			if( module.getName().equals(name) ) {
-				mId = module.getId();
-				System.out.println("Located Id ["+mId+"] for module '"+name+"'");
-				break;
+		if( pId > -1 ) {
+			ListModuleRequest listModuleRequest = new ListModuleRequest();
+			listModuleRequest.withProjectId(pId).withIncludeDescendants(true);
+			List<org.qas.qtest.api.services.project.model.Module> modules = 
+					projectService.listModule(listModuleRequest);
+			for(org.qas.qtest.api.services.project.model.Module module : modules) {
+				if( module.getName().equals(name) ) {
+					mId = module.getId();
+					System.out.println("Located Id ["+mId+"] for module '"+name+"'");
+					break;
+				}
 			}
 		}
 		return mId;
 	}
-
-	public static void observeRetrieveTestCasesFromModule(String name) throws Exception {
+	
+	
+	public static void observeRetrieveTestCaseFromModule(long mId) throws IOException {
 		GetModuleRequest getModuleRequest = 
 				new GetModuleRequest()
-				.withProjectId(projectId)
-				.withModuleId(getModuleId(name))
+				.withProjectId(68329L)
+				.withModuleId(mId)
 				.withIncludeDescendants(true);
 		org.qas.qtest.api.services.project.model.Module module;
 		module = projectService.getModule(getModuleRequest);
-		observeExtractTestCases( module );
-	}
-
-	public static void observeExtractTestCases(org.qas.qtest.api.services.project.model.Module module) throws IOException {
-		System.out.println( "testcase: " + module.getName() + ", order_no: " + module.getOrder() );
-		TestDesignService testDesignService = new TestDesignServiceClient(getCredentials());
-		ListTestCaseRequest listTestCaseRequest = new ListTestCaseRequest();
-		listTestCaseRequest.setProjectId(projectId);
-		listTestCaseRequest.setModuleId(module.getId());
-		List<TestCase> testcases = testDesignService.listTestCase(listTestCaseRequest);
-		if( testcases != null && testcases.size() > 0 ) {
-			System.out.println("Located ["+testcases.size()+"] testcase(s) for '"+module.getName()+"'");	
-			testcases.forEach(item -> {
-				ListTestStepRequest listTestStepRequest = new ListTestStepRequest();
-				listTestStepRequest
-					.withProjectId(projectId)
-					.withTestCaseId(item.getId())
-					.withTestCaseVersion(item.getTestCaseVersionId());
-				List<TestStep> testSteps = testDesignService.listTestStep(listTestStepRequest);
-				int stepsCount = testSteps.size();
-				System.out.println(" name: "+item.getName()+", steps: "+stepsCount);
-			});
-		}
-		List<org.qas.qtest.api.services.project.model.Module> modules = module.getChildren();
-		if( modules != null ) {
-			modules.forEach( item -> {
-				try {
-					observeExtractTestCases( item );
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			});
-		}
-	}
-
-	// - Preliminary - //
-	public static TestCase testcase;
-	public static TestCase observeGetTestCaseByName(String name) throws Exception {
-		ListModuleRequest listModuleRequest = new ListModuleRequest();
-		listModuleRequest.withProjectId(projectId).withIncludeDescendants(true);
-		List<org.qas.qtest.api.services.project.model.Module> modules = 
-				projectService.listModule(listModuleRequest);
-		for(org.qas.qtest.api.services.project.model.Module module : modules) {
-			observeGetTestCase(name, module);
-		}
-		return testcase;
-	}
-
-	public static void observeGetTestCase(String name, 
-			org.qas.qtest.api.services.project.model.Module module) throws Exception {
-		TestDesignService testDesignService = new TestDesignServiceClient(getCredentials());
-		ListTestCaseRequest listTestCaseRequest = new ListTestCaseRequest();
-		listTestCaseRequest.setProjectId(projectId);
-		listTestCaseRequest.setModuleId(module.getId());
-		List<TestCase> testcases = testDesignService.listTestCase(listTestCaseRequest);
-		if( testcases != null && testcases.size() > 0 ) {
-			testcases.forEach(item -> {
-				if(item.getName().equals(name)) {
-					testcase = item;
-					System.out.println("Located '"+item.getName()+"'!");
-					return;
-				}
-			});
-		}
+		module.getChildren().forEach(m -> {
+			for( int i = 0; i<m.getLinks().size(); i++ ) {
+				Link link = m.getLinks().get(i);
+				System.out.println( link );
+			}
+		});
 		
-		if( testcase !=null ) { return; }
-		
-		List<org.qas.qtest.api.services.project.model.Module> modules = module.getChildren();
-		if( modules != null ) {
-			modules.forEach( item -> {
-				try {
-					observeGetTestCase(name, item);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			});
-		}		
 	}
-
+	
+	
+	
 	public static void main(String[] args) throws Exception {
-		// observeRetrieveTestCasesFromModule("To Be Automated");
-		System.out.println(observeGetTestCaseByName("Create Opportunity with a custom opportunity "
-				+ "type and custom Rationale, and add to target list"));
+		// connectToProjectService();		
+		// displayProjects();
+		// observeTestDesignService();
+		// getProjectId("Compass Portal - Beer ");
+		long mId = getModuleId("To Be Automated", 
+				getProjectId("Compass Portal - Beer "));
+		
+		observeRetrieveTestCaseFromModule(mId);
 	}
+
 }
