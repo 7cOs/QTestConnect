38baf2cf645d8b5983f4bae1d5955751ec89374c
7cOs <soko.karneh@gmail.com>
2018-06-21 19:11:50 -0500

    Update of kiksof.cmd including addition of taskkill statement for killing IE

--- qtestConn/conn2qtest/utils/kilsof.cmd
+++ qtestConn/conn2qtest/utils/kilsof.cmd
@@ -2,5 +2,6 @@ taskkill /FI "WINDOWTITLE eq QTestCiController - Compass Portal*" /F
 taskkill /FI "WINDOWTITLE eq Opportunities*" /F
 
 taskkill /im chromedriver.exe /f
+taskkill /im ieDriverServer.exe /f
 
 pause
